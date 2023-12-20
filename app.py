750551361c7debe9ee23d18ddc8fb0dd21cdfbab
Nitin Shrivastav <38551867+nitinshrivastav@users.noreply.github.com>
2023-12-20 20:34:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,request,jsonify
+app=Flask(__name__)
+@app.route('/testing',methods=['POST'])
+def dhjdhj():
+    try:
+        a=int(request.args.get('data1'))
+        b=int(request.args.get('data2'))
+        return jsonify({'answer':a+b}) 
+    except:
+        return jsonify({'Error':'Invalid Data'})
+app.run()
